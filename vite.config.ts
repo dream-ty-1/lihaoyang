dacd79c5a9e2eb71ef1686192e3df42d68c79a19
dream-ty-1 <2810179305@qq.com>
2025-05-27 02:09:11 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
